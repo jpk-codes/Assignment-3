e7a506c008fa8868b580667f5aa073def5977e69
jpk-codes <jayaprakash.b.dev@gmail.com>
2025-06-08 20:49:55 +0530

    Delete Assignment 3 task 1.py

--- Assignment 3 task 1.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * (factorial(n-1))
-
-num = int(input("Enter a number: "))
-
-print("Factorial of", num, "is:", factorial(num))
